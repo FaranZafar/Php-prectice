4b5f0f2267575833e8287e5ad0df612f7fb812e3
Faran <faranpannu02@gmail.com>
2026-04-02 17:50:46 +0500

    php prectice code

---
+++ php-tutorial-and-prectice/day-02.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Day 02 php</title>
+</head>
+<body>
+    <h2>Day two of php</h2>
+    <?php
+    //two ways for output values
+    //1. echo  
+    // -> no return value
+    // -> take multiple parameters
+    // -> faster than print
+    // -> it can used with and without paranthises
+    // examples
+    echo "Hallo<br>";
+    echo ("hallo <br>");
+    echo "<h2>PHP is Fun!</h2><br>";
+    echo "hello wolrd!<br>";
+    echo "I want to learn php<br>";
+
+    // in single quotes variable inserted using . operator
+   
+    //2. print function 
+    // -> has return value 1 so it can be used in expressions
+    // -> take only 1 argument
+    print("<h2>Print function</h2><br>");
+    print "hallo";
+    ?>
+</body>
+</html>
